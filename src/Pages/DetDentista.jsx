416dfc5073ba7a641b225db4d373d2167e3b9c79
Catalina Hernandez <cataxto@gmail.com>
2023-04-06 14:13:23 -0500

    Creando rutas de la App

---
+++ src/Pages/DetDentista.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DetDentista = () => {
+  return (
+    <div>DetDentista</div>
+  )
+}
+
+export default DetDentista
---
+++ src/routes.jsx
@@ -0,0 +1,6 @@
+export const routes = {
+    home: '/',
+    detalleDentista: '/detalleDentista',
+    contacto: '/contacto',
+    destacados: '/destacados', 
+}
